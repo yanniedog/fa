6cc72c115b29d94858089b0372bf7210cb9fa75f
yanniedog <jkokavec@gmail.com>
2023-10-19 11:44:07 +1100

    Update step3-scrape-local-htmls.py
    removed timezone function

--- step3-scrape-local-htmls.py
+++ step3-scrape-local-htmls.py
@@ -2,7 +2,7 @@ import os
 import csv
 import subprocess
 import re
-import requests  # Make sure this import is at the top of your script
+import requests
 from bs4 import BeautifulSoup
 
 def read_config():
@@ -24,13 +24,13 @@ def reformat_date(d):
     return f"{d[6:]}/{d[4:6]}/{d[:4]}"
 
 def extract_url_content(html_content):
-    # Use BeautifulSoup to parse the HTML content and extract the URL
     soup = BeautifulSoup(html_content, 'html.parser')
     url_meta_tag = soup.find('meta', property='og:url')
     if url_meta_tag:
         return url_meta_tag['content']
     else:
         return ''
+
 def extract_data(h, f, rep_fac, icao_to_iata_map):
     s = BeautifulSoup(h, 'html.parser')
     data = []
@@ -38,15 +38,6 @@ def extract_data(h, f, rep_fac, icao_to_iata_map):
     flight_code, original_date = p[1].strip(), p[2].strip()[:-5] if len(p) >= 3 else ("", "")
     date = reformat_date(original_date)
 
-    # Locate and extract the TZ value from the HTML
-    tz_element = s.find('h4', string='Times and Time Zones')
-    tz = ""
-    if tz_element:
-        tz_text = tz_element.find_next('p', class_='indent').text
-        tz_match = re.search(r'All times are in (\w+) time', tz_text)
-        if tz_match:
-            tz = tz_match.group(1)
-    # Extract Departure and Arrival Codes
     url_meta_tag = s.find('meta', property='og:url')
     dep, arr, url_content = "", "", ""
     if url_meta_tag:
@@ -57,7 +48,6 @@ def extract_data(h, f, rep_fac, icao_to_iata_map):
             dep = icao_to_iata_map.get(dep, dep)
             arr = icao_to_iata_map.get(arr, arr)
 
-    # Get shortened URL
     shortened_url = shorten_url(url_content) if url_content else ''
 
     for tr in s.select('tr'):
@@ -66,7 +56,7 @@ def extract_data(h, f, rep_fac, icao_to_iata_map):
             try:
                 row = {
                     'Date': date,
-                    f'Time ({tz})': tr.select_one('td:nth-child(1) span:nth-child(1)').text.strip(),
+                    f'Time': tr.select_one('td:nth-child(1) span:nth-child(1)').text.strip(),
                     'Flight Code': flight_code,
                     'Dep': dep,
                     'Arr': arr,
@@ -83,7 +73,8 @@ def extract_data(h, f, rep_fac, icao_to_iata_map):
                 data.append(row)
             except Exception as e:
                 pass
-    return data, tz
+    return data
+
 def main():
     config = read_config()
     if config is None:
@@ -111,20 +102,19 @@ def main():
         if f.endswith('.html'):
             with open(os.path.join(temp_dir, f), 'r', encoding='utf-8') as file:
                 h = file.read()
-            d, tz = extract_data(h, f, rep_fac, icao_to_iata_map)
+            d = extract_data(h, f, rep_fac, icao_to_iata_map)
             if not d:
                 continue
             csv_filename = f.replace('.html', '.csv')
             try:
                 with open(os.path.join(temp_dir, csv_filename), 'w', newline='') as csvfile:
-                    fieldnames = ['Date', f'Time ({tz})', 'Flight Code', 'Dep', 'Arr', 'Latitude', 'Longitude', 'Course', 'kts', 'km/h', 'Alt(m)', 'Vert rate', 'Reporting Facility', 'URL']
+                    fieldnames = ['Date', 'Time', 'Flight Code', 'Dep', 'Arr', 'Latitude', 'Longitude', 'Course', 'kts', 'km/h', 'Alt(m)', 'Vert rate', 'Reporting Facility', 'URL']
                     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                     writer.writeheader()
                     for row in d:
                         writer.writerow(row)
             except Exception as e:
                 pass
-
 
 def shorten_url(url):
     try:
@@ -134,6 +124,6 @@ def shorten_url(url):
     except Exception as e:
         print(f"An error occurred while shortening the URL: {e}")
     return url
+
 if __name__ == "__main__":
     main()
-
