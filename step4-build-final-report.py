3fb3feb0619f4e3356089e667b5f3e6ea8b99006
yanniedog <jkokavec@gmail.com>
2023-10-19 11:44:36 +1100

    Update step4-build-final-report.py
    removed timezone function

--- step4-build-final-report.py
+++ step4-build-final-report.py
@@ -27,10 +27,10 @@ def main():
     os.makedirs(output_dir, exist_ok=True)
     output_csv = os.path.join(output_dir, 'positions-used-by-flightaware.csv')
     all_rows = []
+
     if os.path.exists(output_csv):
         with open(output_csv, 'r') as f:
             all_rows.extend(csv.DictReader(f))
-    tz = None
 
     for filename in os.listdir(input_dir):
         if filename.endswith('.csv') and 'tracklog_links' not in filename:
@@ -38,43 +38,35 @@ def main():
             formatted_date = f"{date_str[6:8]}/{date_str[4:6]}/{date_str[0:4]}"
             with open(os.path.join(input_dir, filename), 'r') as f:
                 csv_reader = csv.DictReader(f)
-                if tz is None and csv_reader.fieldnames:
-                    time_field = next((field for field in csv_reader.fieldnames if "Time" in field), None)
-                    if time_field:
-                        match = re.match(r'Time \((.*?)\)', time_field)
-                        if match:
-                            tz = match.group(1)
                 for row in csv_reader:
                     row['Date'] = formatted_date
                     all_rows.append(row) if row not in all_rows else None
 
-    if tz is None:
-        print("Note: Time zone ('tz') could not be determined from the tracklog CSVs. This will occur if the current cycle didn't add any new tracklog positions to your report.")
-        return
-
     if all_rows:
-        time_column = [col for col in all_rows[0].keys() if col.startswith("Time")][0] if any(col.startswith("Time") for col in all_rows[0].keys()) else None
-        if time_column:
-            all_rows.sort(key=lambda x: (x['Date'], x[time_column]))
-            with open(output_csv, 'w', newline='') as f:
-                fieldnames = all_rows[0].keys()
-                csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
-                csv_writer.writeheader()
-                for row in all_rows:
-                    csv_writer.writerow(row)
+        if all_rows[0]:
+            time_column = [col for col in all_rows[0].keys() if col.startswith("Time")][0] if any(col.startswith("Time") for col in all_rows[0].keys()) else None
+            if time_column:
+                all_rows.sort(key=lambda x: (x['Date'], x[time_column]))
+                with open(output_csv, 'w', newline='') as f:
+                    fieldnames = all_rows[0].keys()
+                    csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+                    csv_writer.writeheader()
 
-            print("{:<15} {:<18} {:<15} {:<8} {:<8} {:<12} {:<12} {:<10} {:<10} {:<10} {:<10} {:<10} {:<25} {:<40}".format(
-                "Date", f"Time ({tz})", "Flight Code", "Dep", "Arr", "Latitude", "Longitude", "Course",
-                "kts", "km/h", "Alt(m)", "Vert rate", "Reporting Facility", "URL"
-            ))
-            print("-" * 196)  # Add a line under the header row
+                    for row in all_rows:
+                        csv_writer.writerow(row)
 
-            for row in all_rows:
-                print("{:<15} {:<18} {:<15} {:<8} {:<8} {:<12} {:<12} {:<10} {:<10} {:<10} {:<10} {:<10} {:<25} {:<40}".format(
-                    row["Date"], row.get(f"Time ({tz})", ""), row["Flight Code"], row["Dep"], row["Arr"], row["Latitude"],
-                    row["Longitude"], row["Course"], row["kts"], row["km/h"], row["Alt(m)"], row["Vert rate"],
-                    row["Reporting Facility"], row.get("URL", "")
+                print("{:<15} {:<15} {:<15} {:<8} {:<8} {:<12} {:<12} {:<10} {:<10} {:<10} {:<10} {:<10} {:<25} {:<40}".format(
+                    "Date", "Time", "Flight Code", "Dep", "Arr", "Latitude", "Longitude", "Course",
+                    "kts", "km/h", "Alt(m)", "Vert rate", "Reporting Facility", "URL"
                 ))
+                print("-" * 196)  # Add a line under the header row
+
+                for row in all_rows:
+                    print("{:<15} {:<15} {:<15} {:<8} {:<8} {:<12} {:<12} {:<10} {:<10} {:<10} {:<10} {:<10} {:<25} {:<40}".format(
+                        row["Date"], row.get("Time", ""), row["Flight Code"], row["Dep"], row["Arr"], row["Latitude"],
+                        row["Longitude"], row["Course"], row["kts"], row["km/h"], row["Alt(m)"], row["Vert rate"],
+                        row["Reporting Facility"], row.get("URL", "")
+                    ))
 
 if __name__ == "__main__":
     main()
